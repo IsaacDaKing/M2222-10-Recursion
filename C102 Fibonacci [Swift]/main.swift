3c9cf20cea67d6e8e15147343c30a6beac6e6e31
Isaac Pandyan <isaac.pandyan@student.allenisd.org>
2022-11-18 14:37:07 -0600

    initial commit

---
+++ C102 Fibonacci [Swift]/main.swift
@@ -0,0 +1,25 @@
+
+
+// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+let n = 7
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+
+
+// Add your code below:
+// Helped by Reynard Gunawan and Tahmid Rahmen
+func fibo(num: Int) -> Int {
+    print("Calculating fibonacci(\(num))...")
+    if (num <= 2) {
+        print("Terminal case, returning 1.")
+        return 1
+    }
+    else {
+        let run = fibo(num: num-1) + fibo(num: num-2)
+        print("Non-terminal case, returning \(run).")
+        return run
+    }
+}
+
+print("The final result is: \(fibo(num: n)).")
